c7e94cd8a5722e31525df401e8c73f268b50dd47
Surya A <surya.aathiyappan@pearson.com>
2017-10-05 15:50:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/pearson/autobahn/autobahn_tracker_sdk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pearson.autobahn.autobahn_tracker_sdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ trackersdk/src/main/java/com/pearson/autobahn/trackersdk/PETracker.java
@@ -0,0 +1,18 @@
+package com.pearson.autobahn.trackersdk;
+
+import android.util.Log;
+
+/**
+ * TrackerSDK
+ * It will captures user's interactions and send it to Autobahn Data Ecosystem.
+ *
+ * @author GSE Data & Services - gsesoftwareservices@pearson.com
+ * @see https://developer.english.com
+ */
+
+public class PETracker {
+
+    public void init() {
+        Log.i("SDK", "Tracker SDK");
+    }
+}
